0094b209ab02277cc6a0fad71ee0d1ed1074eb21
Interazione su web <85piccio@gmail.com>
2014-09-04 12:51:23 +0200

    edit beacon

--- MuseoAntropologia/AppDelegate.m
+++ MuseoAntropologia/AppDelegate.m
@@ -69,14 +69,14 @@
     //Beacon 1.1
     CLBeaconRegion *region2;
     //qui si definisce la regione dei beacon, questa è quella del beacon 2.
-    region2 = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"EBEFD083-70A2-47C8-9837-E7B5634DF524"] major: 1 minor: 1 identifier: Saccop1];
+    region2 = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"EBEFD083-70A2-47C8-9837-E7B5634DF524"] major: 1 minor: 5 identifier: Saccop1];
     region2.notifyEntryStateOnDisplay = YES;
     [_locationManager startMonitoringForRegion:region2];
     
     //Beacon 1.2
     CLBeaconRegion *region3;
     //qui si definisce la regione dei beacon, questa è quella del beacon 3.
-    region3 = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"EBEFD083-70A2-47C8-9837-E7B5634DF524"] major: 1 minor: 2 identifier: Saccop2];
+    region3 = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"EBEFD083-70A2-47C8-9837-E7B5634DF524"] major: 1 minor: 0 identifier: Saccop2];
     region3.notifyEntryStateOnDisplay = YES;
     [_locationManager startMonitoringForRegion:region3];
     
